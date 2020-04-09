f9626be70985e3dc8c944d9b6d57d355f8cbb2a6
Bhrugu-scientist <61165633+Bhrugu-scientist@users.noreply.github.com>
2020-04-08 19:32:17 -0700

    Update 9) Types of Variables.R

--- 9) Types of Variables.R
+++ 9) Types of Variables.R
@@ -1,31 +1,36 @@
-
+## Types of Variables
 
 #integer
-x <- 2L
+x <- 2L   ## add L after the number to store it as an integer. R by default stores at double level so if you are not going to use it in an arithmetic operation then only add an "L".
 typeof(x)
-
+Output :- "integer"
 
  #double
 y <- 2.5
 typeof(y)
+Output :- "double"
 
 
 x <- 2
 typeof(x)
+Output :- "double"
+
 
 
 #complex number
 z <- 3 +2i
 typeof(z)
+Output :- "complex"
 
 
 #charachter
 a <- "jordan the rockstar"
 typeof(a)
-
+Output :- "character"
 
 #logical
 b <- TRUE
 typeof(b)
+Output :- "logical"
 
 
