061867492765a53252c8d06e674fe98e2b624838
Bhrugu-scientist <61165633+Bhrugu-scientist@users.noreply.github.com>
2020-04-08 19:28:36 -0700

    Add files via upload

---
+++ 9) Types of Variables.R
@@ -0,0 +1,31 @@
+
+
+#integer
+x <- 2L
+typeof(x)
+
+
+ #double
+y <- 2.5
+typeof(y)
+
+
+x <- 2
+typeof(x)
+
+
+#complex number
+z <- 3 +2i
+typeof(z)
+
+
+#charachter
+a <- "jordan the rockstar"
+typeof(a)
+
+
+#logical
+b <- TRUE
+typeof(b)
+
+
