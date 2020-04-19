dd92584b692d229cc90e0cab2f2f15b732249e12
Bhrugu-scientist <61165633+Bhrugu-scientist@users.noreply.github.com>
2020-04-19 01:28:27 -0700

    Create 7) The For loop.R

---
+++ 7) The For loop.R
@@ -0,0 +1,32 @@
+counter <- 1                       ## assigned value "1" to a variable called counter
+while (counter <12) {              ## This code considers number till 12 
+  print(counter)                   
+  counter <- counter + 1           ## Prints counter = 1 and adds a number everytime i.e. counter <- counter + 1
+}
+
+## Output: 
+[1] 1
+[1] 2
+[1] 3
+[1] 4
+[1] 5
+[1] 6
+[1] 7
+[1] 8
+[1] 9
+[1] 10
+[1] 11
+
+
+
+for (i in 1:4) {
+  print("Hello Jordan!")           ## Here we have given the range of 4 i.e. (1:4) which prints "hello Jordan" 4 times
+  }
+ 
+ 
+ 
+ Output:
+  [1] "Hello Jordan!"
+[1] "Hello Jordan!"
+[1] "Hello Jordan!"
+[1] "Hello Jordan!"
