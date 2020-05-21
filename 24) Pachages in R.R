bcc1a1c3ef5b6274a2f2920b4bd4f87fd0b384c3
Bhrugu-scientist <61165633+Bhrugu-scientist@users.noreply.github.com>
2020-05-21 00:04:05 -0700

    Add files via upload

---
+++ 24) Pachages in R.R
@@ -0,0 +1,21 @@
+rnorm(5,mean = 50, sd =2)
+
+c()
+x <- c("a","b","c")
+seq(from = 10, to = 20, length.out = 100)
+A <- seq(from = 10, to = 20, along.with = x )
+A
+rep(5:6,each = 10)
+
+print()
+
+is.numeric(A)
+is.integer(A)
+is.double(A)
+is.character(A)
+
+typeof(A)
+
+sqrt(A)
+paste(A)
+
