a5f3678b6dd73d95d9c4b7c2d2d19fbb8db48193
Bhrugu Thakkar <61165633+Bhrugu-scientist@users.noreply.github.com>
2020-07-25 01:24:59 -0700

    Add files via upload

---
+++ 44) Importing Data in R.R
@@ -0,0 +1,7 @@
+## Method1 Select the file manually
+
+Stats <- read.csv(file.choose())
+Stats
+
+###Method 2 :Set Working Directory and Read Data
+getwd()
---
+++ 46) Using the $ sign.R
@@ -0,0 +1,10 @@
+stats
+Stats
+head(Stats)
+Stats[3,3]
+Stats[3,"Birth.rate"]
+Stats$Internet.users
+Stats$Trend <- Stats$Birth.rate/Stats$Internet.users
+Stats$Trend
+Stats$Internet.users[2]
+levels(Stats$Income.Group)
